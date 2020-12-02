30b373d9745cee75cc92b78c6c016078ca802431
Stuart Brookes <stuart@brookes-net.co.uk>
2020-12-02 23:24:20 +0000

    Minor day 2 refactor

--- C#/Challenge/DayTwo.cs
+++ C#/Challenge/DayTwo.cs
@@ -16,29 +16,34 @@ namespace AoC2020
             //1-3 a: abcde
             while ((CurrentLine = File.ReadLine()) != null)
             {
-                var LineParts = CurrentLine.Split(" ");
-                var MinMax = LineParts[0].Split("-");
-                var MinNumber = Convert.ToInt16(MinMax[0]);
-                var MaxNumber = Convert.ToInt16(MinMax[1]);
-                var Character = LineParts[1][0];
-
-                var CharCount = LineParts[2].Count(x => x == Character);
-
-                if (MinNumber <= CharCount && MaxNumber >= CharCount)
-                {
-                    GoodPasswordsByCount++;
-                }
-
-                if (LineParts[2][MinNumber - 1] == Character
-                    ^ LineParts[2][MaxNumber - 1] == Character)
-                {
-                    GoodPasswordsByPosition++;
-                }
+                CheckLine(CurrentLine, ref GoodPasswordsByCount, ref GoodPasswordsByPosition);
             }
             File.Close();
 
             Console.WriteLine($"Good Passwords by count: {GoodPasswordsByCount}");
             Console.WriteLine($"Good Passwords by position: {GoodPasswordsByPosition}");
+        }
+
+        private static void CheckLine(string lineToCheck, ref int goodPasswordsByCount, ref int goodPasswordsByPosition)
+        {
+            var LineParts = lineToCheck.Split(" ");
+            var MinMax = LineParts[0].Split("-");
+            var MinNumber = Convert.ToInt16(MinMax[0]);
+            var MaxNumber = Convert.ToInt16(MinMax[1]);
+            var Character = LineParts[1][0];
+
+            var CharCount = LineParts[2].Count(x => x == Character);
+
+            if (MinNumber <= CharCount && MaxNumber >= CharCount)
+            {
+                goodPasswordsByCount++;
+            }
+
+            if (LineParts[2][MinNumber - 1] == Character
+                ^ LineParts[2][MaxNumber - 1] == Character)
+            {
+                goodPasswordsByPosition++;
+            }
         }
     }
 }
